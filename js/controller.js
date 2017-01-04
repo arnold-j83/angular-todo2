3d4ec2d20446b6b373f73a1fe0f49edc8e0184ed
John <arnold-j83@sky.com>
2017-01-04 11:43:20 +0000

    heroku commit

--- js/controller.js
+++ js/controller.js
@@ -3,17 +3,26 @@ angular.module('RouteControllers',[])
 		$scope.title = "Angular ToDo App";
 	})
 
-	.controller('RegisterController', function($scope) {
+	.controller('RegisterController', function($scope, UserAPIService) {
 		$scope.title = "Register"; 
 
 		$scope.registrationUser = {};
+
+		var URL = "https://morning-castle-91468.herokuapp.com/";
 
 		$scope.submitForm = function(){
 			if ($scope.registrationForm.$valid) {
 				$scope.registrationUser.username = $scope.user.username;
 				$scope.registrationUser.password = $scope.user.password;
+
+				UserAPIService.registerUser(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+					$scope.data = results.data;
+					alert("You have successfully registered to Angular ToDo");
+				}).catch(function(err) {
+					alert("oops, something went wrong :(");
+				});
 			}
 
-			console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
-		}
+			//console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
+		};
 	});
